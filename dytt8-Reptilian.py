718ca9d65e09a5e658dbc1a0aa57439ed7c4a4b2
horizon <m17759502859@163.com>
2018-05-31 17:46:24 +0800

    第一个爬虫程序

--- dytt8-Reptilian.py
+++ dytt8-Reptilian.py
@@ -1,10 +1,10 @@
 import re
 import requests
 '''
-    第一个爬虫程序，抓取电影天堂“芯片精品”板块的电影下载地址 
+    第一个爬虫程序，抓取电影天堂“新片精品”板块的电影下载地址 
 '''
 content = requests.get('http://www.dytt8.net/')
-content.encoding = 'gb2312'#http://www.dytt8.net/的字符编码
+content.encoding = 'gb2312'                                             #http://www.dytt8.net/的字符编码
 content = re.search('<!--{start:最新影视下载-->.*?<!--}end:最新下载--->', content.text, re.S).group()
 pattern = re.compile("<td.*?>.*?<a.*?</a>.*?<a.*?href='(.*?)'>(.*?)</a><br/>", re.S)
 results = re.findall(pattern, content)
