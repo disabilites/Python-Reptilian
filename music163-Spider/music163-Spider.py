53e8c587d88f832dc82bdc71792fe4179c77ed1b
horizon <m17759502859@163.com>
2018-06-11 12:38:04 +0800

    优化代码结构

--- music163-Spider/music163-Spider.py
+++ music163-Spider/music163-Spider.py
@@ -1,7 +1,6 @@
+from bs4 import BeautifulSoup
 import requests
 import re
-from bs4 import BeautifulSoup
-from selenium import webdriver
 import time
 
 base_url = '歌单URL，如：https://music.163.com/playlist?id=2246544491'
@@ -13,34 +12,42 @@ headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
 }
 
-with open('test.js', 'w', encoding='utf-8') as f:
-    f.write('music = [' + '\n')
+def get_music_sheet():
+    for music in main.find_all('a'):
+        id = re.search('[0-9]+', music['href'])
+        suburl = 'https://music.163.com' + music['href']
+        html = requests.get(suburl, headers=headers).text
+        soup = BeautifulSoup(html, 'lxml')
+        yield soup, music, id
 
-info = {}
-se = requests.session()
-se = BeautifulSoup(se.get(base_url, headers=headers).content, 'lxml')
-main = se.find('ul', {'class': 'f-hide'})
-
-for music in main.find_all('a'):
-    id = re.search('[0-9]+', music['href'])
-    suburl = 'https://music.163.com' + music['href']
-    html = requests.get(suburl, headers=headers).text
-    soup = BeautifulSoup(html, 'lxml')
-    cover = soup.find('img')['src']
+def get_music_info(soup, music, id):
+    global music_str
+    name = music.text
     artist = soup.find_all('a', {'class': 's-fc7'})[1].text
-    info['name'] = music.text
+    url = 'http://music.163.com/song/media/outer/url?id=' + id.group() + '.mp3'
+    cover = soup.find('img')['src']
+    info['name'] = name
     info['artist'] = artist
-    info['url'] = 'http://music.163.com/song/media/outer/url?id=' + id.group() + '.mp3'
+    info['url'] = url
     info['cover'] = cover
-    print(info['name'])
-    print(info['artist'])
-    print(info['url'])
-    print(info['cover'])
-    print('------------------------------------------------------------------------------------------------------')
-    time.sleep(2)
+    music_str += str(info) + ',\n'
+    #time.sleep()
 
-    with open('test.js', 'a', encoding='utf-8') as f:
-        f.write(str(info) + ',\n')
+if __name__ == "__main__":
+    count = 1
+    info = {}
+    music_str = 'music = ' + '\n'
+    se = requests.session()
+    se = BeautifulSoup(se.get(base_url, headers=headers).content, 'lxml')
+    main = se.find('ul', {'class': 'f-hide'})
 
-with open('test.js', 'a', encoding='utf-8') as f:
-    f.write(']')
+    for data in get_music_sheet():
+        get_music_info(data[0], data[1], data[2])
+        print('已抓取' + str(count) + '首')
+        count += 1
+
+    music_str += ']'
+    with open('test.js', 'w', encoding='utf-8') as f:
+            f.write(music_str)
+
+    print('完成！')
