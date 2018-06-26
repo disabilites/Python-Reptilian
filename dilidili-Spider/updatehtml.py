c7b5d8f2d3c6e5c34aa7cc8389487a3906dd4f22
horizon <1043922433@qq.com>
2018-06-26 13:06:15 +0800

    正则表达式更新

--- dilidili-Spider/updatehtml.py
+++ dilidili-Spider/updatehtml.py
@@ -9,8 +9,8 @@ def update():
         html = f.read()
 
     str_html = str(html)
-    video_pattern = re.compile('/.*?\?1')
-    image_pattern = re.compile('/.*?\.jpg')
+    video_pattern = re.compile('href="/anime')
+    image_pattern = re.compile('src="/uploads')
     li_attr = str(re.search('<li class=.*"elmnt-.*?"', html).group())
     video_pos = 0
     image_pos = 0
@@ -18,16 +18,18 @@ def update():
 
     while video_pattern.search(html, video_pos):
         video_str = video_pattern.search(html, video_pos)
-        str_html = str_html.replace(video_str.group(), 'http://www.dilidili.wang' + video_str.group())
+        str_html = str_html.replace(video_str.group(), 'href="http://www.dilidili.wang/anime')
         video_pos = video_str.span()[1]
 
     while image_pattern.search(html, image_pos):
         image_str = image_pattern.search(html, image_pos)
-        str_html = str_html.replace(image_str.group(), 'http://www.dilidili.wang' + image_str.group())
+        str_html = str_html.replace(image_str.group(), 'src="http://www.dilidili.wang/uploads')
         image_pos = image_str.span()[1]
 
     with open('result.html', 'w', encoding='utf-8') as f:
         f.write(str_html)
 
     logging.info(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '：页面属性替换成功！')
+
+update()
 
