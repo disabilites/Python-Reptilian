351bfd24f2ebd693249b8808d2a8eafa751ef4ea
horizon <horizon@ashtwo.cn>
2019-01-20 11:58:12 +0800

    主程序模块

---
+++ proxy_pool/main.py
@@ -0,0 +1,15 @@
+from getter import Getter
+from tester import Tester
+
+
+def run_getter():
+    getter = Getter()
+    getter.run()
+
+def run_tester():
+    tester = Tester()
+    tester.run()
+
+if __name__ == '__main__':
+    run_getter()
+    run_tester()
