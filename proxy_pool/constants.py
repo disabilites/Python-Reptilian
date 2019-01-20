1c9be1725594847d499dbc72664439116a9ced11
horizon <horizon@ashtwo.cn>
2019-01-20 12:04:24 +0800

    常量模块

---
+++ proxy_pool/constants.py
@@ -0,0 +1,16 @@
+#db.py
+MAX_SCORE = 100
+MIN_SCORE = 0
+INITIAL_SCORE = 10
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'username'
+MYSQL_PASSWORD = 'password'
+MYSQL_TABLE = 'tablename'
+
+#getter.py
+MAX_POOL_COUNT = 10000
+
+#tester.py
+VALID_STATUS_CODES = [200]
+TEST_URL = 'http://www.baidu.com'
+BATCH_TEST_SIZE = 100
